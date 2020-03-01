37b3a9bab1485a4a0ea9a9b4bb012b44324f7676
Summerdise <350464517@qq.com>
2020-03-01 23:55:17 +0800

    [zmc] feat: complete all function

--- io/src/main/java/com/thoughtworks/io/FileUtil.java
+++ io/src/main/java/com/thoughtworks/io/FileUtil.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.io;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileUtil {
 
@@ -13,6 +12,46 @@ public class FileUtil {
      * 例如把a文件夹(a文件夹下有1.txt和一个空文件夹c)复制到b文件夹，复制完成以后b文件夹下也有一个1.txt和空文件夹c
      */
     public static void copyDirectory(File from, File to) throws IOException {
+        if (to.exists()) {
+            deleteAllFile(to);
+        } else {
+            to.mkdir();
+        }
+        File[] fromList = from.listFiles();
+        if (fromList != null) {
+            for (File f : fromList) {
+                File fromFile = new File(from.getPath() + File.separator + f.getName());
+                File toFile = new File(to.getPath() + File.separator + f.getName());
+                if (f.isDirectory()) {
+                    copyDirectory(fromFile, toFile);
+                } else {
+                    copyFile(fromFile, toFile);
+                }
+            }
+        }
+    }
 
+    public static void deleteAllFile(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                deleteAllFile(f);
+            }
+        } else {
+            System.out.println("delete文件：" + file.delete());
+        }
+    }
+
+    public static void copyFile(File from, File to) throws IOException {
+        try {
+            FileInputStream input = new FileInputStream(from);
+            FileOutputStream output = new FileOutputStream(to);
+            int n;
+            while ((n = input.read()) != -1) {
+                output.write(n);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
